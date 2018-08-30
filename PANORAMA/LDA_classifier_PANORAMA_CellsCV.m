c8a6d2a3b710a8b00b49c00b829e05b143df4b97
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2018-08-30 11:38:21 +0200

    Update LDA_PANORAMA

--- PANORAMA/LDA_classifier_PANORAMA_CellsCV.m
+++ PANORAMA/LDA_classifier_PANORAMA_CellsCV.m
@@ -9,16 +9,15 @@ SamplesData=struct('Data',[],'Labels',{});
 H=dir(fullfile('Samples\', '*.csv'));
 SamplesFiles = cellstr(char(H(1:end).name));
 
-H=dir(fullfile('Labels\', '*.xlsx'));
+H=dir(fullfile('Labels\', '*.csv'));
 LabelsFiles = cellstr(char(H(1:end).name));
 clear H
 
 for i=1:length(SamplesFiles)
     SamplesData(i).Data = csvread(['Samples\' SamplesFiles{i}]);
-    [~,txt]=xlsread(['Labels\' LabelsFiles{i}]);
-    SamplesData(i).Labels = txt;
+    SamplesData(i).Labels = table2cell(readtable(['Labels\' LabelsFiles{i}],'ReadVariableNames',0,'Delimiter',','));
 end
-clear i SamplesFiles LabelsFiles txt
+clear i SamplesFiles LabelsFiles
 
 Data=[];
 Labels = [];
@@ -27,7 +26,9 @@ for i=1:length(SamplesData)
     Labels = [Labels; SamplesData(i).Labels];
 end
 clear i
-%% run LDA Classifier
+
+% Data is already arcsinh(5) transformed
+%% run LDA Classifier with 5-fold cross-validation
 
 CVO = cvpartition(Labels,'k',5);
 Accuracy = zeros(CVO.NumTestSets,1);
@@ -46,15 +47,17 @@ for i = 1:CVO.NumTestSets
     
     tic
     Predictor = predict(classificationLDA,Data(teIdx,:));
+    testing_time(i)=toc;           %in seconds
     Accuracy(i) = nnz(strcmp(Predictor,Labels(teIdx)))/size(Labels(teIdx),1);
     ConfusionMat = ConfusionMat + confusionmat(Labels(teIdx),Predictor,'order',CellTypes);
-    testing_time(i)=toc;           %in seconds
+    
 end
 Total_time = sum(training_time)+sum(testing_time);
-cvAcc = mean(Accuracy)*100;
-cvSTD = std(Accuracy)*100;
 training_time = mean(training_time);
 testing_time = mean(testing_time);
+cvAcc = mean(Accuracy)*100;
+cvSTD = std(Accuracy)*100;
+disp(['LDA Accuracy = ' num2str(cvAcc) ' ' char(177) ' ' num2str(cvSTD) ' %'])
 clear i Predictor classificationLDA trIdx teIdx CVO Accuracy
 %% Performance evaluation
 
@@ -66,16 +69,22 @@ MedianFmeasure = median(Fmeasure);
 Subset_size = sum(ConfusionMat,2);
 WeightedFmeasure = (Subset_size./size(Data,1))'*Fmeasure;
 
+disp(['Median F1-score = ' num2str(MedianFmeasure)])
+figure,scatter(log10(Subset_size),Fmeasure,100,'filled'),title('PANORAMA')
+xlabel('Log10(population size)'),ylabel('F1-score'),box on, grid on
 %% Population Frequency
 
 True_Freq = sum(ConfusionMat,2)./sum(sum(ConfusionMat));
 Predicted_Freq = sum(ConfusionMat,1)'./sum(sum(ConfusionMat));
-figure,bar([True_Freq Predicted_Freq])
-% ticklabels=CellTypes;
-% ticklabels = cellfun(@(x) strrep(x,' ','\newline'), ticklabels,'UniformOutput',false);
-xticks(1:22)
+Max_Freq_diff = max(abs(True_Freq-Predicted_Freq))*100;
+
+disp(['delta_f = ' num2str(Max_Freq_diff)])
+figure,bar([True_Freq*100 Predicted_Freq*100])
+xticks(1:24)
 xticklabels(CellTypes)
 xtickangle(90)
 set(gca,'FontSize',10)
-set(gca,'XLim',[0 23])
+set(gca,'XLim',[0 25])
 legend({'True','Predicted'},'FontSize',10)
+legend show
+ylabel('Freq. %'),title('PANORAMA')
