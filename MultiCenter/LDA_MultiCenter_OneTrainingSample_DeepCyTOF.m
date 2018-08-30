1b5fdc35fd8ea16426331ec35121e7e931af0caa
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2018-08-30 13:47:12 +0200

    Update LDA_MultiCenter_DeepCyTOF

--- MultiCenter/LDA_MultiCenter_OneTrainingSample_DeepCyTOF.m
+++ MultiCenter/LDA_MultiCenter_OneTrainingSample_DeepCyTOF.m
@@ -23,33 +23,49 @@ for i=1:length(SamplesData)
     Labels = [Labels; SamplesData(i).Labels];
 end
 clear i
-%% run LDA Classifier
-tic
+%% run LDA Classifier with Sample no.2 as training as per DeepCyTOF
+
 CellTypes = unique(Labels);
+tic
+% training is excluding class 0 'unlabeled'
 classificationLDA = fitcdiscr(...
-    SamplesData(2).Data, ...
-    SamplesData(2).Labels);
+    SamplesData(2).Data(SamplesData(2).Labels~=0,:), ...
+    SamplesData(2).Labels(SamplesData(2).Labels~=0));
+training_time=toc;          %in seconds
 Accuracy = zeros(length(SamplesData),1);
 WeightedFmeasure = zeros(length(SamplesData),1);
+testing_time = zeros(length(SamplesData),1);
 for i = 1:length(SamplesData)
-    
-    Predictor = predict(classificationLDA,SamplesData(i).Data);
+    tic
+    [Predictor,scores] = predict(classificationLDA,SamplesData(i).Data);
+    Current_Scores = max(scores,[],2);
+    Predictor(Current_Scores < 0.4)=0;   % prob < 0.4 = class 0 'unlabeled'
+    testing_time(i)=toc;
     Accuracy(i) = nnz((Predictor(SamplesData(i).Labels~=0)==SamplesData(i).Labels(SamplesData(i).Labels~=0)))/size(SamplesData(i).Labels(SamplesData(i).Labels~=0),1);
     ConfusionMat = confusionmat(SamplesData(i).Labels,Predictor,'order',CellTypes);
+    col1 = ConfusionMat(2:end,1);
+    ConfusionMat = ConfusionMat(2:end,2:end);
     % F1 measure
     Precision = diag(ConfusionMat)./sum(ConfusionMat,1)';
-    Recall = diag(ConfusionMat)./sum(ConfusionMat,2);
+    Recall = diag(ConfusionMat)./(sum(ConfusionMat,2)+col1);
     Fmeasure = 2 * (Precision.*Recall)./(Precision+Recall);
     % MedianFmeasure = median(Fmeasure);
-    Subset_size = sum(ConfusionMat,2);
-    WeightedFmeasure(i) = (Subset_size(2:end)./sum(Subset_size(2:end)))'*Fmeasure(2:end);
+    Subset_size = sum(ConfusionMat,2)+col1;
+    WeightedFmeasure(i) = (Subset_size./sum(Subset_size))'*Fmeasure;
 end
-Total_time = toc;           %in seconds
+clear i Predictor classificationLDA ConfusionMat Precision Recall Fmeasure
 WeightedFmeasure(2)=[];
 Accuracy(2)=[];
 
 MeanWeightedFmeasure=mean(WeightedFmeasure);
-MeanAccuracy=mean(Accuracy);
-StdAccuracy=std(Accuracy);
+cvAcc = mean(Accuracy)*100;
+cvSTD = std(Accuracy)*100;
+disp(['LDA Accuracy = ' num2str(cvAcc) ' ' char(177) ' ' num2str(cvSTD) ' %'])
+disp(['Weighted F1-score = ' num2str(MeanWeightedFmeasure)])
 
-clear i Predictor classificationLDA ConfusionMat Precision Recall Fmeasure 
+% Supplementary Fig. S2
+figure,bar(WeightedFmeasure)
+xticklabels([1 3:16])
+set(gca,'YLim',[0.7 1])
+xlabel('Samples')
+ylabel('Weighted F1-score'),title('Multi-Center LDA Performance')
